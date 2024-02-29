a25f87cc5ddd57fdfe5f72a9d2812bbc2eb8d85d
ameya-6964 <belvalkar7@gmail.com>
2024-02-29 17:40:53 +0530

    Added Navbar To root Layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse | Find Perfect Rental Appartment",
@@ -17,7 +18,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );

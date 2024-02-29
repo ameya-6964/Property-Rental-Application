ac592671916898b76e981d055ba44c332ad8c020
ameya-6964 <belvalkar7@gmail.com>
2024-02-29 17:20:55 +0530

    Understood next 14 Navigation Features

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+"use client";
 
 const PropertyPage = () => {
-  return <h1 className="text-[2rem] font-bold">I Am a Single Property</h1>;
+  return (
+    <div>
+      <h1 className="text-[2rem] font-bold">I Am Property With Id </h1>
+
+      <button className="bg-blue-500 p-2">Go Home</button>
+    </div>
+  );
 };
 
 export default PropertyPage;
